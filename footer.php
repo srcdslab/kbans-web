b5d3b8b38b3d8754af54a999ba3ba33c5f32dd27
Rushaway <rushaway@hotmail.fr>
2023-09-14 01:54:04 +0200

    fix: footer display

--- footer.php
+++ footer.php
@@ -8,12 +8,10 @@
             ?>
           <span><i class="fas fa-code"></i> Integration made by <a href="https://github.com/Dolly132" title="Coded by Dolly" target="_blank" rel="noopener">Dolly</a></span>
           <span>Co-Edited by <a href="https://github.com/Rushaway/" title="Rushaway's GitHub Profile" target="_blank" rel="noopener">.Rushaway </a></span>
-          <br/>
-          <span>Powered by <a href="https://github.com/srcdslab/sm-plugin-KnockbackRestrict/tree/main" title="KnockbackRestrict Repository" target="_blank" rel="noopener">KnockbackRestrict 3.3.3</a></span>
-          <span>This Website is open source, check it out <a href="https://github.com/srcdslab/kbans-web" title="Kbans website Repository" target="_blank" rel="noopener">here</a></span>
         </div>
         <div class="flex flex-fd:column text:right">
-            <p></p>
+            <p>Powered by <a href="https://github.com/srcdslab/sm-plugin-KnockbackRestrict/tree/main" title="KnockbackRestrict Repository" target="_blank" rel="noopener">KnockbackRestrict</a></p>
+          <p>This Website is open source, check it out <a href="https://github.com/srcdslab/kbans-web" title="Kbans website Repository" target="_blank" rel="noopener">here</a></p>
         </div>
       </div>
 </footer>
