725783c4ba0be1dc13a75099226752ba8dbd8ef7
Rushaway <rushaway@hotmail.fr>
2024-09-14 09:04:40 +0200

    major fix: Prevent SQL injection and bad behaviour
    * fix(js): Encode every URI component
    * fix(php): Sanitize dangerous field, use prepared statements
    * fix(php): Sanitize everything

--- functions_url.php
+++ functions_url.php
@@ -1,30 +1,38 @@
 <?php
     include_once('connect.php');
     include_once('functions_global.php');
-    
-    if(isset($_GET['id']) && !isset($_GET['reban']) && !isset($_GET['edit'])) {
-        $id = $_GET['id'];
+
+    function sanitizeString($input) {
+        // Replace problematic characters with an empty string
+        $replacements = array("'", '"', "\\", ";", "`", "--", "#", "=", ">", "<", "&", "%", "|", "^", "~", "(", ")");
+        $sanitized = str_replace($replacements, "", $input);
+        return htmlspecialchars($sanitized, ENT_QUOTES, 'UTF-8'); // Escape HTML entities
+    }
+
+    if (isset($_GET['id']) && !isset($_GET['reban']) && !isset($_GET['edit'])) {
+        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
         showKbanInfo($id);
     }
 
-    if(isset($_GET['oldid']) && !isset($_GET['reban']) && !isset($_GET['edit'])) {
-        if(!isset($_COOKIE['steamID'])) {
+    if (isset($_GET['oldid']) && !isset($_GET['reban']) && !isset($_GET['edit'])) {
+        if (!isset($_COOKIE['steamID'])) {
             die();
         }
 
         $admin = new Admin();
         $admin->UpdateAdminInfo($_COOKIE['steamID']);
 
-        $id = $_GET['oldid'];
+        $id = filter_input(INPUT_GET, 'oldid', FILTER_SANITIZE_NUMBER_INT);
         
         $kban = new Kban();
         $info = $kban->getKbanInfoFromID($id);
-        if(!IsAdminLoggedIn() || (!$admin->DoesHaveFullAccess() && $info['admin_steamid'] != $admin->adminSteamID)) {
+        if (!IsAdminLoggedIn() || (!$admin->DoesHaveFullAccess() && $info['admin_steamid'] != $admin->adminSteamID)) {
             die();
         }
-        $reason = $_GET['reason'];
-        $kban = new Kban();
-        if(!$kban->UnbanByID($id, $reason)) {
+        
+        $reason = sanitizeString(filter_input(INPUT_GET, 'reason', FILTER_SANITIZE_STRING));
+        
+        if (!$kban->UnbanByID($id, $reason)) {
             die();
         } 
 
@@ -32,52 +40,52 @@
     }
 
     function showKbanInfo(int $id) {
-       GetRowInfo($id);
+        GetRowInfo($id);
     }
 
-    if(isset($_GET['add']) && isset($_GET['playerName'])) {
-        if(!IsAdminLoggedIn()) {
+    if (isset($_GET['add']) && isset($_GET['playerName'])) {
+        if (!IsAdminLoggedIn()) {
             die();
         }
 
-        $playerName = $_GET['playerName'];
-        $playerSteamID = $_GET['playerSteamID'];
-        $length = $_GET['length'];
-        $reason = $_GET['reason'];
+        $playerName = sanitizeString(filter_input(INPUT_GET, 'playerName', FILTER_SANITIZE_STRING));
+        $playerSteamID = sanitizeString(filter_input(INPUT_GET, 'playerSteamID', FILTER_SANITIZE_STRING));
+        $length = filter_input(INPUT_GET, 'length', FILTER_SANITIZE_NUMBER_INT);
+        $reason = sanitizeString(filter_input(INPUT_GET, 'reason', FILTER_SANITIZE_STRING));
 
         $icon = "<i class='fa-solid fa-xmark'></i>&nbsp";
-        if(empty($playerName)) {
+        if (empty($playerName)) {
             echo "<p>$icon Player name cannot be empty!</p>";
             die();
         }
 
-        if(empty($playerSteamID)) {
+        if (empty($playerSteamID)) {
             echo "<p>$icon Player SteamID cannot be empty!</p>";
             die();
         }
 
-        if(!preg_match("/^STEAM_[0-5]:[01]:\d+$/", $playerSteamID)) {
+        if (!preg_match("/^STEAM_[0-5]:[01]:\d+$/", $playerSteamID)) {
             echo "<p>$icon Invalid SteamID Format</p>";
             die();
         }
 
-        if(empty($reason)) {
+        if (empty($reason)) {
             echo "<p>$icon Reason cannot be empty!</p>";
             die();
         }
 
-        if(str_contains($playerName, "'") || str_contains($playerName, "\"")) {
+        if (str_contains($playerName, "'") || str_contains($playerName, "\"")) {
             echo "<p>$icon ' and \" characters cannot be used for Player Name!</p>";
             die();
         }
 
-        if(str_contains($reason, "'") || str_contains($reason, "\"")) {
+        if (str_contains($reason, "'") || str_contains($reason, "\"")) {
             echo "<p>$icon ' and \" characters cannot be used for Reason!</p>";
             die();
         }
 
         $kban = new Kban();
-        if($kban->IsSteamIDAlreadyBanned($playerSteamID)) {
+        if ($kban->IsSteamIDAlreadyBanned($playerSteamID)) {
             echo "<p>$icon $playerSteamID is already kbanned!</p>";
             die();
         }
@@ -85,65 +93,65 @@
         $kban->addNewKban($playerName, $playerSteamID, $length, $reason);
     }
 
-    if(isset($_GET['edit']) && isset($_GET['playerName'])) {
-        if(!isset($_COOKIE['steamID'])) {
+    if (isset($_GET['edit']) && isset($_GET['playerName'])) {
+        if (!isset($_COOKIE['steamID'])) {
             die();
         }
 
-        $id = $_GET['id'];
-        $playerName = $_GET['playerName'];
-        $playerSteamID = $_GET['playerSteamID'];
-        $length = $_GET['length'];
-        $reason = $_GET['reason'];
+        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+        $playerName = sanitizeString(filter_input(INPUT_GET, 'playerName', FILTER_SANITIZE_STRING));
+        $playerSteamID = sanitizeString(filter_input(INPUT_GET, 'playerSteamID', FILTER_SANITIZE_STRING));
+        $length = filter_input(INPUT_GET, 'length', FILTER_SANITIZE_NUMBER_INT);
+        $reason = sanitizeString(filter_input(INPUT_GET, 'reason', FILTER_SANITIZE_STRING));
 
         $icon = "<i class='fa-solid fa-xmark'></i>&nbsp";
-        if(empty($playerName)) {
+        if (empty($playerName)) {
             echo "<p>$icon Player name cannot be empty!</p>";
             die();
         }
 
-        if(empty($playerSteamID)) {
+        if (empty($playerSteamID)) {
             echo "<p>$icon Player SteamID cannot be empty!</p>";
             die();
         }
 
-        if(!preg_match("/^STEAM_[0-5]:[01]:\d+$/", $playerSteamID)) {
+        if (!preg_match("/^STEAM_[0-5]:[01]:\d+$/", $playerSteamID)) {
             echo "<p>$icon Invalid SteamID Format</p>";
             die();
         }
 
-        if(empty($reason)) {
+        if (empty($reason)) {
             echo "<p>$icon Reason cannot be empty!</p>";
             die();
         }
 
-        if(str_contains($playerName, "'") || str_contains($playerName, "\"")) {
+        if (str_contains($playerName, "'") || str_contains($playerName, "\"")) {
             echo "<p>$icon ' and \" characters cannot be used for Player Name!</p>";
             die();
         }
 
-        if(str_contains($reason, "'") || str_contains($reason, "\"")) {
+        if (str_contains($reason, "'") || str_contains($reason, "\"")) {
             echo "<p>$icon ' and \" characters cannot be used for Reason!</p>";
             die();
         }
-        
+
         $admin = new Admin();
         $admin->UpdateAdminInfo($_COOKIE['steamID']);
 
-        if($length == 0 && !$admin->DoesHaveFullAccess()) {
+        if ($length === null && !$admin->DoesHaveFullAccess()) {
             echo "<p>$icon You do not have permission for Permanent bans!</p>";
             die();
         }
 
-        if(empty($reason)) {
+        if (empty($reason)) {
             $reason = "NO REASON";
         }
 
-        if(empty($length)) {
+        if ($length === null) {
             $length = 0;
         }
         
-        if($length < 0) {
+        if ($length < 0) {
             $length = 30;
         }
 
@@ -151,40 +159,40 @@
 
         $info = $kban->getKbanInfoFromID($id);
 
-        if(!IsAdminLoggedIn() || (!$admin->DoesHaveFullAccess() && $info['admin_steamid'] != $admin->adminSteamID)) {
+        if (!IsAdminLoggedIn() || (!$admin->DoesHaveFullAccess() && $info['admin_steamid'] != $admin->adminSteamID)) {
             die();
         }
 
-        if($playerName == $info['client_name'] && $playerSteamID == $info['client_steamid'] && $reason == $info['reason'] && $length == ($info['length'] * 60)) {
+        if ($playerName == $info['client_name'] && $playerSteamID == $info['client_steamid'] && $reason == $info['reason'] && $length == ($info['length'] * 60)) {
             echo "<p>$icon Cannot detect any changes to edit!</p>";
             die();
         }
 
-        if(!$kban->IsSteamIDAlreadyBanned($playerSteamID)) {
-            echo "<p>$icon The edited steamid is alread kbanned and cannot be edited from here</p>";
+        if (!$kban->IsSteamIDAlreadyBanned($playerSteamID)) {
+            echo "<p>$icon The edited steamid is already kbanned and cannot be edited from here</p>";
             die();
         }
 
         $time_stamp_end = ($info['time_stamp_start'] + $length);
         if ($length > 0 && $time_stamp_end < time()) {
-            echo "<p>$icon Invalid Duration! Expected a duration that will last in the future but got a one that has already ended.</p>";
+            echo "<p>$icon Invalid Duration! Expected a duration that will last in the future but got one that has already ended.</p>";
             die();
         }
         $kban->EditKban($id, $playerName, $playerSteamID, $length, $reason);
     }
 
-    if(isset($_GET['delete'])) {
-        if(!isset($_COOKIE['steamID'])) {
+    if (isset($_GET['delete'])) {
+        if (!isset($_COOKIE['steamID'])) {
             die();
         }
         
         $admin = new Admin();
         $admin->UpdateAdminInfo($_COOKIE['steamID']);
-        if(!IsAdminLoggedIn() || !$admin->DoesHaveFullAccess()) {
+        if (!IsAdminLoggedIn() || !$admin->DoesHaveFullAccess()) {
             die();
         }
 
-        $id = $_GET['deleteid'];
+        $id = filter_input(INPUT_GET, 'deleteid', FILTER_SANITIZE_NUMBER_INT);
         $kban = new Kban();
         $kban->RemoveKbanFromDB($id);
         die();
