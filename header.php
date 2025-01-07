c41846434c4be48a1dfe3c7e591eb48f511940d6
Rushaway <rushaway@hotmail.fr>
2025-01-07 11:01:44 +0100

    chore(deps): Bump mustache to 4.2.0

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
         setcookie("secret_key", "", 1, "/", $_SERVER['SERVER_NAME'], true, true);
         setcookie("aid", "", 1, "/", $_SERVER['SERVER_NAME'], true, true);
     }
-?>
+?>e
 
 <head>
     <title>KnockBack Bans</title>
@@ -29,7 +29,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/3.0.0/mustache.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/4.2.0/mustache.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="js/kbans.js"></script>
 </head>
