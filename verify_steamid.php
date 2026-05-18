1f522d6d18e90eeb18665a096f875ea8a711f1d9
Rushaway <rushaway@hotmail.fr>
2026-05-18 10:02:35 +0200

    Use null coalescing operator for steamid

--- verify_steamid.php
+++ verify_steamid.php
@@ -2,7 +2,7 @@
 include('steam.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $steamid = $_POST['steamid'];
+    $steamid = $_POST['steamid'] ?? '';
     $steam = new Steam();
     $result = $steam->verifyAndConvertSteamID($steamid);
     echo json_encode($result);
